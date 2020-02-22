b2f6297bbc43938548de0dcf794dd34d6542e5e6
xiaobai <592151648@qq.com>
2020-02-22 19:07:11 +0800

    feat: 轮播图片组件支持

--- src/main.js
+++ src/main.js
@@ -3,13 +3,18 @@ import App from './App.vue'
 import router from './router'
 // click 300ms延迟问题
 import fastClick from 'fastclick'
+// 窗口滑动
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 // 手机像素边框问题
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'https://b1-q.mafengwo.net/s15/M00/71/03/CoUBGV5ObLqALH09AB_TNctu4-0320.png?imageMogr2%2Finterlace%2F1'
+      }, {
+        id: '002',
+        imgUrl: 'https://p1-q.mafengwo.net/s15/M00/F1/D5/CoUBGV5NBYaAHJkXACSOOvUzwoY815.png?imageMogr2%2Finterlace%2F1'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff !important
+  .wrapper
+    overflow hidden
+    width 100%
+    height: 0
+    padding-bottom 31.25%
+    background #eee
+    .swiper-img
+      width 100%
+</style>>
